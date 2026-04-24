24331760168c8ef4d4e68a78d7f536637ba77160
Hasyakb <Hassanyakububabagasa@gmail.com>
2026-04-24 18:10:56 +0100

    Fix distPath to use __dirname for static files

--- server.ts
+++ server.ts
@@ -1050,8 +1050,9 @@ async function startServer() {
     });
     app.use(vite.middlewares);
   } else {
-    const distPath = path.join(process.cwd(), "dist");
+    const distPath = path.join(__dirname, 'dist');
     app.use(express.static(distPath));
+    app.use('/assets', express.static(path.join(distPath, 'assets')));
     app.get("*", (req, res) => {
       res.sendFile(path.join(distPath, "index.html"));
     });
