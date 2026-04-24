d2e94239d849b0e17f36ed5daabac03ee3741046
Hasyakb <Hassanyakububabagasa@gmail.com>
2026-04-24 17:10:38 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -49,7 +49,7 @@ const upload = multer({ storage });
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ extended: true, limit: '10mb' }));
